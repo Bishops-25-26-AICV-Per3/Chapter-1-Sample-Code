2b9698a4d7fd948c30ee487f18e0cf21ce853b79
Marcus Jaiclin <marcus.jaiclin@bishops.com>
2025-09-18 11:35:50 -0700

    Version from Per 5

--- argparse_details.py
+++ argparse_details.py
@@ -2,7 +2,23 @@ import argparse
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="Path to image")
-ap.add_argument("-H", "--height", type=int, help="Height")
+ap.add_argument("-w", "--width", type=int, help="Width of output")
+# DON'T USE -h!! That is reserved for help.
+ap.add_argument("-H", "--height", type=int, help="Height of output")
 args = ap.parse_args()
 
+print(args)
+print(args.image)
+print(args.width)
 print(args.height)
+
+"""
+With command line: (pr python3, YMMV)
+python argparse_details.py --image x.png -w 50 -H 100
+
+you should get:
+Namespace(image='x.png', width=50, height=100)
+x.png
+50
+100
+"""
